d52d8e2359569e26a5b94675d3828fcac572c650
Sheuh Xiong <sheuhxiong@gmail.com>
2021-09-15 19:50:08 -0400

    added navbar component, added scss styling to navbar

---
+++ src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import "./Styles/Navbar.css";
+
+function Navbar() {
+  return (
+    <nav className="navbar">
+      <a href="/">Home</a>
+      <a href="/">Blog Posts</a>
+    </nav>
+  );
+}
+
+export default Navbar;
