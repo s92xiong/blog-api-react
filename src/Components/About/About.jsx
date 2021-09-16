5013d75324ef9d04e4b455887b1ad877fed70db6
Sheuh Xiong <sheuhxiong@gmail.com>
2021-09-16 17:38:15 -0400

    Added new font: PT Serif for span content in About component

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import "./Styles/About.css";
 
 function About(props) {
 
@@ -6,7 +7,11 @@ function About(props) {
 
   return (
     <div className="about">
-      <h1>About {name}</h1>
+      <div>
+        <h1>About {name}</h1>
+        <span>Aspiring Full Stack Developer currently learning and building projects via The Odin Project. Sharing my interests and experiences through this blog.</span>
+      </div>
+      <img src="https://pbs.twimg.com/profile_images/968698422751518720/zeP0peZp_400x400.jpg" alt="/" />
     </div>
   );
 }
