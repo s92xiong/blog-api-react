d61cfe347b29204c16e538267f782e1630fe6b25
Sheuh Xiong <sheuhxiong@gmail.com>
2021-09-16 17:05:21 -0400

    renamed Card component to BlogList component, updated About component

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import { useEffect, useState } from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import './App.css';
 import About from './Components/About/About';
-import Card from './Components/Card/Card';
+import BlogList from './Components/BlogList/BlogList';
 import Navbar from './Components/Navbar/Navbar';
 import loadBlogData from './Javascripts/loadBlogData';
 
@@ -20,15 +20,15 @@ function App() {
         <Navbar name="Sheuh Xiong" />
         <Switch>
           <Route
+            // Homepage
             exact
             path="/"
-            render={(props) => (<Card {...props} blogData={blogData} />)}
+            render={(props) => (<BlogList {...props} blogData={blogData} />)}
           />
           <Route
             path="/about"
             render={(props) => (<About {...props} name="Sheuh Xiong" />)}
           />
-
         </Switch>
       </div>  
     </BrowserRouter>
--- src/Components/BlogList/BlogList.jsx
+++ src/Components/BlogList/BlogList.jsx
@@ -1,17 +1,17 @@
 import React from 'react';
 import "./Styles/Card.css";
 
-function Card(props) {
+function BlogList(props) {
   const { blogData } = props;
 
   if (!blogData) return <>...Loading</>;
   
   return (
-    <div className="card-container">
+    <div className="blog-list-container">
       {
         blogData.map((blog, i) => {
           return (
-            <div className="card" key={i}>
+            <div className="blog-card" key={i}>
               <h1>{blog.title}</h1>
               <p>{blog.text}</p>
             </div>
@@ -22,4 +22,4 @@ function Card(props) {
   );
 }
 
-export default Card;
+export default BlogList;
