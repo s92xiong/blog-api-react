b102a042641b8acf3afd5bf828305c4b90abf6f7
Sheuh Xiong <sheuhxiong@gmail.com>
2021-09-16 18:13:48 -0400

    Added styling and proper positioning to About component and its child elements

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -7,9 +7,9 @@ function About(props) {
 
   return (
     <div className="about">
-      <div>
+      <div className="about-info">
         <h1>About {name}</h1>
-        <span>Aspiring Full Stack Developer currently learning and building projects via The Odin Project. Sharing my interests and experiences through this blog.</span>
+        <span>Full Stack Developer currently learning and building projects via The Odin Project. Sharing my interests and experiences through this blog.</span>
       </div>
       <img src="https://pbs.twimg.com/profile_images/968698422751518720/zeP0peZp_400x400.jpg" alt="/" />
     </div>
