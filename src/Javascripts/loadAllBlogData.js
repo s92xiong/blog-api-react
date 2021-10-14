cee86a9b7bb5dfbe83def78e86f4c571809a36e9
Sheuh Xiong <sheuhxiong@gmail.com>
2021-10-14 14:26:00 -0400

    fixed bug with Blog List dates being undefined

--- src/Javascripts/loadAllBlogData.js
+++ src/Javascripts/loadAllBlogData.js
@@ -2,22 +2,20 @@ const load_all_blog_data = async (setBlogData) => {
   const response = await fetch("https://enigmatic-bastion-98317.herokuapp.com/");
   const json_data = await response.json();
 
-  const blog_data = json_data.map(obj => {
-    // Shorten text, cut after 2 setences.
+  const json_data_updated_time = json_data.map(obj => {
+    
+    // Shorten text to two paragraphs only, append 3 periods to end of string
     let string_arr = obj.text.split(". ");
     string_arr.splice(2);
-    let new_string = string_arr.join(". ");
+    obj.text = `${string_arr.join(". ")}...`;
 
-    obj.text = `${new_string}...`;
-
-    // Update 
-    const array_dates = new Date(obj.timestamp).toString().split(". ");
-    const date_month_day = `${array_dates[1]} ${array_dates[2]}`; 
-
+    // Adjust date, return date as "Sep 10"
+    const array_dates = new Date(obj.timestamp).toString().split(" ");
+    const date_month_day = `${array_dates[1]} ${array_dates[2]}`;
     return {...obj, date_month_day};
   });
 
-  setBlogData(blog_data);
+  setBlogData(json_data_updated_time);
 };
 
 export default load_all_blog_data;
