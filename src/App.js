05fb9933db46c27c0efc06495e93a0475b761a81
Sheuh Xiong <sheuhxiong@gmail.com>
2021-10-16 08:06:56 -0400

    Added project to firebase with build folder

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import './App.css';
 import About from './Components/About/About';
